d6028d2d40668560877c83da5ff9ee921654ca28
Kayden <kayden.adams@colorado.edu>
2021-06-30 20:45:07 -0600

    Adds an item_type var to Item

--- scenes/Item/Item.gd
+++ scenes/Item/Item.gd
@@ -4,13 +4,23 @@ extends Area2D
 func _ready():
 	pass
 
+onready var item_type
+
+
 func randomize_item_type(rng):
 	var rand_float = rng.randf()
+	print(rand_float)
 	# Special
-	if rand_float > 0.8:
+	if rand_float > 0.9:
+		item_type = "apple"
 		$ItemSprite.animation = "special"
-		$ItemSprite.frame = rng.randi_range(0, $ItemSprite.frames.get_frame_count("special") - 1)
+		$ItemSprite.frame = 0
+	elif rand_float >= 0.8 and rand_float < 0.9:
+		item_type = "grimy"
+		$ItemSprite.animation = "special"
+		$ItemSprite.frame = 1
 	else:
+		item_type = "normal"
 		$ItemSprite.animation = "normal"
 		$ItemSprite.frame = rng.randi_range(0, $ItemSprite.frames.get_frame_count("normal") - 1)
 		
@@ -21,9 +31,9 @@ func move(new_pos, tween, speed):
 	tween.start()
 
 func points():
-	if $ItemSprite.animation == "special" and $ItemSprite.frame == 0:
+	if item_type == "apple":
 		return 50
-	elif $ItemSprite.animation == "special" and $ItemSprite.frame == 1:
+	elif item_type == "grimy":
 		return -50
 	else:
 		return 20
